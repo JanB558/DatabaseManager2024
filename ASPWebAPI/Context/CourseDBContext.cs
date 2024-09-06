1a08d0df0254a7bdc352af1f0dbe1a07c352aa79
JanB558 <72133201+JanB558@users.noreply.github.com>
2024-09-06 14:48:05 +0200

    added dbcontext

---
+++ ASPWebAPI/Context/CourseDBContext.cs
@@ -0,0 +1,23 @@
+﻿using ASPWebAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ASPWebAPI.Context
+{
+    public class CourseDBContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        public CourseDBContext(DbContextOptions<CourseDBContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Person>().ToTable("Person");
+            modelBuilder.Entity<Course>().ToTable("Course");
+        }
+    }
+}
