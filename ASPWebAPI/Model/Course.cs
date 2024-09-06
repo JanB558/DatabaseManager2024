c167a381b80ff1b4b652c5e3cf850239f4f23be4
JanB558 <72133201+JanB558@users.noreply.github.com>
2024-09-06 14:41:08 +0200

    added models to API

---
+++ ASPWebAPI/Model/Course.cs
@@ -0,0 +1,8 @@
+﻿namespace ASPWebAPI.Model
+{
+    public class Course
+    {
+        public int ID { get; set; }
+        public string CourseName { get; set; }
+    }
+}
---
+++ ASPWebAPI/Model/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace ASPWebAPI.Model
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int CourseID { get; set; }
+    }
+}
