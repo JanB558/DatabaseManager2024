bf35f5528d67626e1b68987600b542466542461c
JanB558 <72133201+JanB558@users.noreply.github.com>
2024-09-09 14:10:45 +0200

    added service

---
+++ ASPWebAPI/Services/ISQLServerService.cs
@@ -0,0 +1,17 @@
+﻿using ASPWebAPI.Model;
+
+namespace ASPWebAPI.Services
+{
+    public interface ISQLServerService
+    {
+        public IQueryable<Person> GetPeople();
+        public Person GetPersonByID(int id);
+        public void UpdatePerson(Person person);
+        public void DeletePerson(int id);
+        //
+        public IQueryable<Course> GetCourses();
+        public Course GetCourseByID(int id);
+        public void UpdateCourse(Course course);
+        public void DeleteCourse(int id);
+    }
+}
