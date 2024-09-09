42f860d74181954cf228196058643f1b9d9eef74
JanB558 <72133201+JanB558@users.noreply.github.com>
2024-09-09 15:15:39 +0200

    crud for people

--- ASPWebAPI/Program.cs
+++ ASPWebAPI/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<CourseDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// CONNECTION STRING SHOULD NOT BE ON GITHUB ITS JUST AN EXAMPLE
 
 builder.Services.AddScoped<ISQLServerService, SQLServerService>();
 
@@ -34,11 +35,19 @@ app.MapGet("/person/{id}", async (int id, ISQLServerService sqlService) =>
     Results.Ok(await sqlService.GetPersonAsync(id) is Person person
     ? Results.Ok(person) : Results.NotFound()));
 // ADD person
-app.MapPost("/persons", async (Person person, ISQLServerService sqlService) =>
+app.MapPost("/person", async (Person person, ISQLServerService sqlService) =>
 {
     var createdPerson = await sqlService.AddPersonAsync(person);
     return Results.Created($"/persons/{createdPerson.ID}", createdPerson);
 });
+// UPDATE person
+app.MapPut("/person", async (Person person, ISQLServerService sqlService) =>
+    await sqlService.UpdatePersonAsync(person)
+    ? Results.Ok(person) : Results.NotFound());
+// DELETE person
+app.MapDelete("/person/{id}", async (int id, ISQLServerService sqlService) =>
+    await sqlService.DeletePersonAsync(id)
+        ? Results.Ok() : Results.NotFound());
 
 // courses
 
