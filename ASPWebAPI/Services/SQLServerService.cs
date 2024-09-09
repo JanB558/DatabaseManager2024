37a438adc09103d6274a5c18b7779039a6343600
JanB558 <72133201+JanB558@users.noreply.github.com>
2024-09-09 14:51:50 +0200

    finished person service

--- ASPWebAPI/Services/SQLServerService.cs
+++ ASPWebAPI/Services/SQLServerService.cs
@@ -12,52 +12,65 @@ namespace ASPWebAPI.Services
         {
             _context = context;
         }
-
+        //people
         public async Task<IEnumerable<Person>> GetPeopleAsync()
         {
             return await _context.Person.Include(x => x.Course).ToListAsync();
         }
-
-        public Task<bool> AddPersonAsync(Person person)
+        public async Task<Person> GetPersonAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _context.Person
+                .Include(x => x.Course)
+                .Where(p => p.ID == id)
+                .FirstOrDefaultAsync();
         }
+        public async Task<bool> AddPersonAsync(Person person)
+        {
+            await _context.Person.AddAsync(person);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        public async Task<bool> UpdatePersonAsync(Person person)
+        {
+            var personToUpdate = await _context.Person.FindAsync(person.ID);
 
+            if (personToUpdate is null) return false;
+
+            personToUpdate.FirstName = person.FirstName;
+            personToUpdate.LastName = person.LastName;
+            personToUpdate.CourseID = person.CourseID;
+
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+        public async Task<bool> DeletePersonAsync(int id)
+        {
+            var person = await _context.Person.FindAsync(id);
+
+            if (person is null) return false;
+
+            _context.Person.Remove(person);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+        //course
         public Task<bool> DeleteCourseAsync(int id)
         {
             throw new NotImplementedException();
         }
-
-        public Task<bool> DeletePersonAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
         public Task<Course> GetCourseAsync(int id)
         {
             throw new NotImplementedException();
         }
-
         public Task<IEnumerable<Course>> GetCoursesAsync()
         {
             throw new NotImplementedException();
-        }
-
-        
-
-        public Task<Person> GetPersonAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
+        }     
         public Task<bool> UpdateCourseAsync(Course course)
         {
             throw new NotImplementedException();
-        }
-
-        public Task<bool> UpdatePersonAsync(Person person)
-        {
-            throw new NotImplementedException();
-        }
+        }  
     }
 }
