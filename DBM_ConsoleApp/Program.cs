3f11bade7f85282132aaf85ba78f9e8c9feb95e9
JanB558 <72133201+JanB558@users.noreply.github.com>
2024-10-15 15:24:32 +0200

    basic api requests in console app

--- DBM_ConsoleApp/Program.cs
+++ DBM_ConsoleApp/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DBM_ConsoleApp;
+using System;
+using System.Net.Http.Json;
+
+namespace DBM_ConsoleApp
+{
+    internal class Program
+    {
+        static HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            client.BaseAddress = new Uri("https://localhost:7233");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await GetAllCoursesAsync();
+            Console.WriteLine(response);
+            Console.ReadKey();
+        }
+
+        static async Task<string> AddCourseAsync(Course course)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync("/course", course);
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            return responseBody;
+        }
+
+        static async Task<string> GetAllCoursesAsync()
+        {
+            HttpResponseMessage response = await client.GetAsync("/course");
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            return responseBody;
+        }
+    }
+}
