cb45b1b389bf093bb0890af37e4dc8df6e4e0acd
JanB558 <72133201+JanB558@users.noreply.github.com>
2024-10-15 15:01:00 +0200

    added models to console app

---
+++ DBM_ConsoleApp/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBM_ConsoleApp
+{
+    internal class Course
+    {
+        public int ID { get; set; }
+        public required string CourseName { get; set; }
+    }
+}
---
+++ DBM_ConsoleApp/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBM_ConsoleApp
+{
+    internal class Person
+    {
+        public int ID { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public int CourseID { get; set; }
+
+        public Course? Course { get; set; }
+    }
+}
