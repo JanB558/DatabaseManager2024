90ba12634b013291c82c7270c51e49864e7e57aa
JanB558 <72133201+JanB558@users.noreply.github.com>
2024-10-16 15:56:14 +0200

    console app update

--- DBM_ConsoleApp/Program.cs
+++ DBM_ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DBM_ConsoleApp;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Net.Http.Json;
 
@@ -27,7 +28,7 @@ namespace DBM_ConsoleApp
             var run = true;
             while (run)
             {
-                Console.WriteLine("-------------------------------------------------------------------------------------------\n");
+                Console.WriteLine("-------------------------------------------------------------------------------------------");
                 Console.WriteLine("1 - Get all courses\n2 - Get course by ID\n3 - Add new course\n4 - Delete course\n0 - Quit");
                 var key = Console.ReadKey();
                 string result = string.Empty;
@@ -42,6 +43,19 @@ namespace DBM_ConsoleApp
                         var courses = ConvertJsonToCourse(result);
                         PrintCourses(courses);
                         break;
+                    case "2":
+                        Console.Write("ID:");
+                        var id = Console.ReadLine();
+                        int idInt;
+                        if (id is null || id.Equals(String.Empty) || id.Equals("") || !int.TryParse(id, out idInt))
+                        {
+                            Console.WriteLine("Incorrect input."); break;
+                        }
+                        result = await GetCourseByIdAsync(idInt);
+                        Console.WriteLine(result);
+                        var course = ConvertJsonToCourse(result);
+                        PrintCourses(course);
+                        break;
                     case "3":
                         Console.Write("Course Name:");
                         var name = Console.ReadLine();
@@ -49,8 +63,8 @@ namespace DBM_ConsoleApp
                         {
                             Console.WriteLine("Incorrect input."); break;
                         }
-                        Course course = new() { CourseName = name };
-                        result = await AddCourseAsync(course);
+                        Course courseToAdd = new() { CourseName = name };
+                        result = await AddCourseAsync(courseToAdd);
                         Console.WriteLine(result);
                         break;
                     default:
@@ -72,9 +86,7 @@ namespace DBM_ConsoleApp
         {
             HttpResponseMessage response = await client.PostAsJsonAsync("/course", course);
             response.EnsureSuccessStatusCode();
-
             string responseBody = await response.Content.ReadAsStringAsync();
-
             return responseBody;
         }
 
@@ -82,16 +94,43 @@ namespace DBM_ConsoleApp
         {
             HttpResponseMessage response = await client.GetAsync("/course");
             response.EnsureSuccessStatusCode();
-
             string responseBody = await response.Content.ReadAsStringAsync();
+            return responseBody;
+        }
 
+        static async Task<string> GetCourseByIdAsync(int id)
+        {
+            HttpResponseMessage response = await client.GetAsync($"/course/{id}");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
             return responseBody;
         }
 
         static ICollection<Course> ConvertJsonToCourse(string json)
         {
-            var result = JsonConvert.DeserializeObject<List<Course>>(json);
-            return result ?? throw new NullReferenceException();
+            var jsonObject = JObject.Parse(json);
+
+            if (jsonObject["statusCode"] != null)
+            {
+                var result = jsonObject.ToObject<CourseWrapper>();
+                if (result is null) throw new NullReferenceException();
+                List<Course> courses = new List<Course>();
+                courses.Add(result.Value);
+                return courses;
+            }
+            else
+            {
+                var result = jsonObject.ToObject<ICollection<Course>>();
+                if (result is null) throw new NullReferenceException();
+                return result;
+            }
+            //var result = JsonConvert.DeserializeObject<CourseWrapper>(json);
+            //if (result is null) throw new NullReferenceException();
+            //if (result.Value != null) return result.Value;
+            
+            ////fallback
+            //var resultAlt = JsonConvert.DeserializeObject<ICollection<Course>>(json);
+            //return resultAlt ?? throw new NullReferenceException();
         }
     }
 }
