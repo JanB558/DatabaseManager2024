335cf56c7180b90b54325f605429b1699fde0d8a
JanB558 <72133201+JanB558@users.noreply.github.com>
2024-10-17 15:38:48 +0200

    added basic api call

--- ASPWebApp/Controllers/CourseController.cs
+++ ASPWebApp/Controllers/CourseController.cs
@@ -4,10 +4,28 @@ namespace ASPWebApp.Controllers
 {
     public class CourseController : Controller
     {
-        [Route("Course/Course")]
-        public IActionResult Index()
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+        private readonly string _apiUrl;
+        public CourseController(HttpClient httpClient, IConfiguration configuration)
         {
-            return View("../DBM/Course");
+            _httpClient = httpClient;
+            _configuration = configuration;
+            _apiUrl = _configuration.GetConnectionString("ApiConnectionString");
+        }
+
+        [Route("Course/Course")]
+        public async Task<IActionResult> Index()
+        {
+            var endpoint = $"{_apiUrl}/course";
+            HttpResponseMessage response = await _httpClient.GetAsync(endpoint);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Ok(content);
+            }
+            return StatusCode((int)response.StatusCode, "Error calling the API");
+            //return View("../DBM/Course");
         }
     }
 }
