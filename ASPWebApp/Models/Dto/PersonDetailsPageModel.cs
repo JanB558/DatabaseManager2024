46836d0837ccb501438d90d01360973e0ba10306
JanB558 <72133201+JanB558@users.noreply.github.com>
2025-01-07 15:39:00 +0100

    wip details page for person

---
+++ ASPWebApp/Models/Dto/PersonDetailsPageModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ASPWebApp.Models.Dto
+{
+    public class PersonDetailsPageModel
+    {
+        public Person? Person { get; set; }
+        public List<Course> CourseList { get; set; } = [];
+    }
+}
