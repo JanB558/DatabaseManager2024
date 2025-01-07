80c965f43d84277dec366a015673d6b42e81461e
JanB558 <72133201+JanB558@users.noreply.github.com>
2025-01-07 12:51:21 +0100

    course model update

--- ASPWebApp/Models/Course.cs
+++ ASPWebApp/Models/Course.cs
@@ -1,8 +1,13 @@
-﻿namespace ASPWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPWebApp.Models
 {
     public class Course
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
         public required string CourseName { get; set; }
+        public byte[] VersionStamp { get; set; } = [];
     }
 }
